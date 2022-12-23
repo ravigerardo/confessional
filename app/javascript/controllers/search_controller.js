f956820c2677dd0b47349bc1462057bf26158071
Ravi Gerardo <ravishgerardo@gmail.com>
2022-12-23 12:06:49 -0600

    Style for user search

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -6,15 +6,18 @@ export default class extends Controller {
   connect() {
     let modal = new Modal(search_modal)
     this.search().then(users => this.set_users_in_list(users))
-    search_button.addEventListener('click', () => modal.show())
-    q_username_cont.addEventListener('keyup', () => {
+    search_button.addEventListener('click', () => {
+      modal.show()
+      q_username_or_name_cont.focus()
+    })
+    q_username_or_name_cont.addEventListener('keyup', () => {
       this.search().then(users => this.set_users_in_list(users))
     })
   }
 
   async search() {
     let url = new URL(window.location.origin + '/users/search')
-    let params = { 'q[username_cont]': q_username_cont.value }
+    let params = { 'q[username_or_name_cont]': q_username_or_name_cont.value }
     url.search = new URLSearchParams(params).toString()
 
     return fetch(url).then(res => res.json())
@@ -25,13 +28,28 @@ export default class extends Controller {
   set_users_in_list(users) {
     let users_list_items = []
     users.forEach(user => {
-      let element = document.createElement('a')
-      let text = document.createTextNode(user.username)
-      element.appendChild(text)
-      element.classList.add('list-group-item', 'list-group-item-action')
-      element.href = '/' + user.username
-      users_list_items.push(element)
+      let user_item = this.generete_user_item_for_list(user)
+      users_list_items.push(user_item)
     })
     users_list.replaceChildren(...users_list_items);
+  }
+
+  generete_user_item_for_list(user) {
+    let user_item = document.createElement('a')
+
+    let p_tag = document.createElement('p')
+    p_tag.appendChild(document.createTextNode(user.name))
+    p_tag.classList.add('mb-0', 'fw-bold')
+
+    let em_tag = document.createElement('em')
+    em_tag.appendChild(document.createTextNode(user.username))
+
+    user_item.appendChild(p_tag)
+    user_item.appendChild(em_tag)
+
+    user_item.classList.add('list-group-item', 'list-group-item-action')
+    user_item.href = '/' + user.username
+
+    return user_item
   }
 }
